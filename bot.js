4a6e58922c4341150ffcca63390a8686793e9576
Omertis <36013948+Omertis@users.noreply.github.com>
2018-06-20 08:07:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,30 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+
+
+
+
+client.on('ready', () => {
+  console.log('Kffdf98079');
+});
+
+client.on('message', message => {
+  if (message.content === 'pip') {
+      if (message.author.id !== '444155387130675210') return message.react('⚒')
+    const channel = message.member.voiceChannel;
+
+    channel.join()
+    .then(connection => console.log('Connected!'))
+    .catch(console.error);
+  }
+});
+
 
 
 client.login(process.env.BOT_TOKEN);
